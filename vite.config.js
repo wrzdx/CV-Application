718f44f7595bbb894db385838889d7ecf9451967
Evgenii <7zhhovalyg@mail.ru>
2025-12-17 11:20:21 +0300

    remove base for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CV-Application/',
+  // base: '/CV-Application/',
   plugins: [react()],
 })
